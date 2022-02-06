0058a4827014a209d6afb98ba0d27b4c58957862
siva1334 <98871466+siva1334@users.noreply.github.com>
2022-02-06 18:45:11 +0530

    Add files via upload

---
+++ Problem set 1/Problem1.java
@@ -0,0 +1,16 @@
+package com.practice;
+import java.util.*;
+
+public class Problem1 {
+	
+	public static void main ( String args[]) {
+		
+		Scanner scan = new Scanner (System.in);
+		
+		long mobile_no = scan.nextLong(); //used long to avoid integer overflow
+		
+		System.out.println(mobile_no);
+		
+	}
+
+}
