adfa3d41eadefed58382d7463f6dfe030634b3b8
siva1334 <98871466+siva1334@users.noreply.github.com>
2022-02-06 18:48:05 +0530

    Add files via upload

---
+++ Problem set 1/Problem2.java
@@ -0,0 +1,21 @@
+package com.practice;
+import java.util.*;
+
+public class Problem2 {
+	
+		
+
+	public static void main( String args[]) {
+		
+		// used byte for less data usage
+		
+		for ( byte i = 1; i<=10 ; i++ ) {
+			
+			System.out.print(i + " ");
+			
+		}
+		
+		
+		
+	}
+}
