6dfbca5c2f57d11d004863bc07e72ff49c0299c0
siva1334 <98871466+siva1334@users.noreply.github.com>
2022-02-08 14:20:54 +0530

    Add files via upload

---
+++ Problem set 1/Problem68.java
@@ -0,0 +1,75 @@
+package com.practice;
+
+import java.util.Scanner;
+
+// multiply matrix by passing parameters.
+public class Problem68 {
+	
+	private static void multiplyMatrix(int a[][], int b[][], int fc,int fr,int sc,int sr) {
+		
+		int res[][] = new int[fr][sc];
+		
+		for(int i = 0 ; i < fr   ; i++) {
+			
+			for(int j = 0 ; j < sc; j++ ) {
+				
+				int sum = 0;				
+				
+			for(int k = 0 ; k < fc; k++) {
+				
+				sum += (a[i][k] * b[k][j]);
+			}				
+			res [i][j] = sum;
+			}
+		}
+		
+		//printing matrix
+		System.out.println("the multiplied matrix is: ");
+		
+		for(int i  = 0;i < res.length; i++) {
+			
+			for(int j = 0 ; j < res[0].length;j++) {
+				
+			System.out.print(res[i][j] + " ");
+			
+			}
+			System.out.println();
+		}
+	}
+public static void  main(String[] args) {	
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter the first matrix row: ");
+		int fr = scan.nextInt();
+		System.out.println("enter the first matrix colum: ");
+		int fc = scan.nextInt();
+		System.out.println("enter the first matrix row: ");
+		int sr = scan.nextInt();
+		System.out.println("enter the first matrix colum: ");
+		int sc = scan.nextInt();
+		
+		int a[][] = new int [fr][fc];
+		int b[][] = new int[sr][sc];
+		
+		System.out.println(" enter the first matrix: ");
+		for(int i = 0; i < fr; ++i) {
+			for(int j = 0; j < fc; ++j) {
+				a[i][j] = scan.nextInt();
+			}
+		}
+		System.out.println("enter the second matrix: ");
+		for(int i = 0; i < sr; ++i) {
+			for(int j = 0; j < sc; ++j) {
+				b[i][j] = scan.nextInt();
+			}
+		}
+			
+		
+		if(fc != sr) {
+			System.out.println("invalid matrix ");
+		}
+		else {
+			multiplyMatrix(a,b,fc,fr,sc,sr);
+		}
+}
+}
