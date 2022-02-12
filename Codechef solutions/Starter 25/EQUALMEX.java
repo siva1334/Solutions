b783d38bcf550807b64ca78ce81041f2c00d1c17
siva1334 <98871466+siva1334@users.noreply.github.com>
2022-02-12 16:08:45 +0530

    Update EQUALMEX.java

--- Codechef solutions/Starter 25/EQUALMEX.java
+++ Codechef solutions/Starter 25/EQUALMEX.java
@@ -1,3 +1,13 @@
+/*
+The MEX (minimum excluded) of an array is the smallest non-negative integer that does not belong to the array. For instance:
+
+The MEX of [2,2,1] is 0 because 0 does not belong to the array.
+The MEX of [3,1,0,1] is 2 because 0 and 1 belong to the array, but 2 does not.
+The MEX of [0,3,1,2] is 4 because 0,1,2 and 3 belong to the array, but 4 does not.
+You're given an array A containing 2⋅N integers where 0≤Ai≤N.
+Is it possible to reorder the elements of the array in such a way that the MEX of the first N elements is equal to the MEX of the last N elements?
+*/
+
 /* package codechef; // don't place package name! */
 
 import java.util.*;
