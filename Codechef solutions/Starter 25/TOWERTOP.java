de73bf80f2abbb60b96dcce75637ce715ac4bd06
siva1334 <98871466+siva1334@users.noreply.github.com>
2022-02-12 16:00:47 +0530

    Update TOWERTOP.java

--- Codechef solutions/Starter 25/TOWERTOP.java
+++ Codechef solutions/Starter 25/TOWERTOP.java
@@ -1,3 +1,14 @@
+/*
+Chef is given a contract to build towers in Chefland which are made by stacking blocks one above the other. 
+Initially, there is only 1 block in the inventory and no tower has been built. Chef follows the following 2 steps in each operation:
+
+Either build a new tower or update an existing tower that has been built in previous operations using any number of blocks currently present in the inventory. After this step, the size of the inventory reduces by the number of blocks used.
+Suppose the tower Chef updated or built has B blocks after the step, Chef gets to add B new blocks to the inventory as a reward.
+Find the maximum number of towers of height X that Chef can build in M operations.
+
+Note: Tower of height X means that the tower consists of X blocks placed one above the other.
+*/
+
 /* package codechef; // don't place package name! */
 
 import java.util.*;
