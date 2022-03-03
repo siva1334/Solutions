cc69f1abb18fa399685df939319872432f1c1014
siva1334 <98871466+siva1334@users.noreply.github.com>
2022-03-03 12:10:00 +0530

    Create SelfdefenceTraining.java

---
+++ Codechef solutions/Starter28/SelfdefenceTraining.java
@@ -0,0 +1,41 @@
+/*
+After the phenomenal success of the 36th Chamber of Shaolin, 
+San Te has decided to start 37th Chamber of Shaolin. The aim this time is to equip women with shaolin self-defence techniques.
+
+The only condition for a woman to be eligible for the special training is that she must be between 10 and 60 years of age, inclusive of both 10 and 60.
+
+Given the ages of N women in his village, please help San Te find out how many of them are eligible for the special training.
+*/
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    static Scanner scan = new Scanner(System.in);
+    
+    static void solve(){
+        
+        int n = scan.nextInt();
+       int cnt = 0;
+        for(int i = 0; i < n; i++){
+            int x = scan.nextInt();
+            
+            if(x >= 10 && x <= 60)cnt++;
+        }
+        System.out.println(cnt);
+        
+    }
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes 
+		int t = scan.nextInt();
+		while(t-- > 0){
+		    solve();
+		}
+	}
+}
