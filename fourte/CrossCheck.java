af87486d80975d3ac1960f7480abf9b3510d24cd
siva1334 <98871466+siva1334@users.noreply.github.com>
2022-04-01 14:26:58 +0530

    Update CrossCheck.java

--- fourte/CrossCheck.java
+++ fourte/CrossCheck.java
@@ -1,130 +1,131 @@
-package com.practice;
+package com.fourte;
 
 public class CrossCheck {
-
+	
 	int target;
-
-	CrossCheck(int t) {
+	
+	CrossCheck(int t){
 		target = t;
 	}
-
-	void plus(int a[], int val, int val1, int val2, String c1) {
-
-		String temp = "equation = ";
-
-		if (val + (a[2] + a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "+" + "(" + a[2] + "+" + a[3] + ")";
-		}
-		if (val + (a[2] - a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "+" + "(" + a[2] + "-" + a[3] + ")";
-		}
-		if (val + (a[2] * a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "+" + "(" + a[2] + "*" + a[3] + ")";
-		}
-		if (a[2] >= a[3] && a[2] % a[3] == 0 && val + (a[2] / a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "+" + "(" + a[2] + "/" + a[3] + ")";
-		}
-		int x = (a[2] * 10) + a[3];
-		if (val + x == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "+" + "(" + a[2] + "" + a[3] + ")";
-		}
-
-		if (!temp.equals("equation = ")) {
-			System.out.println(temp);
-			System.exit(0);
-		}
-
+	
+void plus(int a[] , int val , int val1, int val2, String c1 ) {
+		
+	String temp = "equation = ";
+	int x = (a[2] * 10) + a[3];
+	
+	if(val + (a[2] + a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "+" + "(" + a[2] + "+" + a[3] + ")";
 	}
-
-	void minus(int a[], int val, int val1, int val2, String c1) {
-		String temp = "equation = ";
-
-		if (val - (a[2] + a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "-" + "(" + a[2] + "+" + a[3] + ")";
-		}
-		if (val - (a[2] - a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "-" + "(" + a[2] + "-" + a[3] + ")";
-		}
-		if (val - (a[2] * a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "-" + "(" + a[2] + "*" + a[3] + ")";
-		}
-		if (a[2] >= a[3] && a[2] % a[3] == 0 && val - (a[2] / a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "-" + "(" + a[2] + "/" + a[3] + ")";
-		}
-		int x = (a[2] * 10) + a[3];
-		if (val - x == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "-" + "(" + a[2] + "" + a[3] + ")";
-		}
-
-		if (!temp.equals("equation = ")) {
-			System.out.println(temp);
-			System.exit(0);
-		}
-
+	else if(val + (a[2] - a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "+" + "(" + a[2] + "-" + a[3] + ")";
 	}
-
-	void multiple(int a[], int val, int val1, int val2, String c1) {
-
-		String temp = "equation = ";
-
-		if (val * (a[2] + a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "*" + "(" + a[2] + "+" + a[3] + ")";
-		}
-		if (val * (a[2] - a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "*" + "(" + a[2] + "-" + a[3] + ")";
-		}
-		if (val * (a[2] * a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "*" + "(" + a[2] + "*" + a[3] + ")";
-		}
-		if (a[2] >= a[3] && a[2] % a[3] == 0 && val * (a[2] / a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "*" + "(" + a[2] + "/" + a[3] + ")";
-		}
-		int x = (a[2] * 10) + a[3];
-		if (val * x == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "*" + "(" + a[2] + "" + a[3] + ")";
-		}
-
-		if (!temp.equals("equation = ")) {
-			System.out.println(temp);
-			System.exit(0);
-		}
-
+	else if(val + (a[2] * a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "+" + "(" + a[2] + "*" + a[3] + ")";
 	}
-
-	void divide(int a[], int val, int val1, int val2, String c1) {
-
-		String temp = "equation = ";
-
-		if (val >= (a[2] + a[3]) && val % (a[2] + a[3]) == 0 && val / (a[2] + a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "/" + "(" + a[2] + "+" + a[3] + ")";
-		}
-		if (a[2] > a[3] && val >= (a[2] - a[3]) && val % (a[2] - a[3]) == 0 && val / (a[2] - a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "/" + "(" + a[2] + "-" + a[3] + ")";
-		}
-		if (val >= (a[2] * a[3]) && val % (a[2] * a[3]) == 0 && val / (a[2] * a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "/" + "(" + a[2] + "*" + a[3] + ")";
-		}
-		if (a[2] >= a[3] && a[2] % a[3] == 0 && val >= (a[2] / a[3]) && val % (a[2] / a[3]) == 0
-				&& val / (a[2] / a[3]) == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "/" + "(" + a[2] + "/" + a[3] + ")";
-		}
-		int x = (a[2] * 10) + a[3];
-		if (val >= x && val % x == 0 && val / x == target) {
-			temp += "(" + val1 + c1 + val2 + ")" + "/" + "(" + a[2] + "" + a[3] + ")";
-		}
-
-		if (!temp.equals("equation = ")) {
-			System.out.println(temp);
-			System.exit(0);
-		}
+	else if(a[2] >= a[3] && a[2] % a[3] == 0 && val + (a[2] / a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "+" + "(" + a[2] + "/" + a[3] + ")";
 	}
+	
+	else if(val + x == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "+" + "(" + a[2] + "" + a[3] + ")";
+	}
+	
+	if(!temp.equals("equation = ")) {
+	System.out.println(temp);
+	//System.exit(0);
+	}
+	
+	}
+void minus(int a[] , int val , int val1, int val2, String c1 ) {
+String temp = "equation = ";
+int x = (a[2] * 10) + a[3];
+	
+	if(val - (a[2] + a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "-" + "(" + a[2] + "+" + a[3] + ")";
+	}
+	else if(val - (a[2] - a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "-" + "(" + a[2] + "-" + a[3] + ")";
+	}
+	else if(val - (a[2] * a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "-" + "(" + a[2] + "*" + a[3] + ")";
+	}
+	else if(a[2] >= a[3] && a[2] % a[3] == 0 && val - (a[2] / a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "-" + "(" + a[2] + "/" + a[3] + ")";
+	}
+	
+	else if(val - x == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "-" + "(" + a[2] + "" + a[3] + ")";
+	}
+	
+	if(!temp.equals("equation = ")) {
+		System.out.println(temp);
+		//System.exit(0);
+		}
+	
+}
+void multiple(int a[] , int val , int val1, int val2, String c1 ) {
+	
+String temp = "equation = ";
+int x = (a[2] * 10) + a[3];
+	
+	if(val * (a[2] + a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "*" + "(" + a[2] + "+" + a[3] + ")";
+	}
+	else if(val * (a[2] - a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "*" + "(" + a[2] + "-" + a[3] + ")";
+	}
+	else if(val * (a[2] * a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "*" + "(" + a[2] + "*" + a[3] + ")";
+	}
+	else if(a[2] >= a[3] && a[2] % a[3] == 0 && val * (a[2] / a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "*" + "(" + a[2] + "/" + a[3] + ")";
+	}
+	
+	else if(val * x == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "*" + "(" + a[2] + "" + a[3] + ")";
+	}
+	
+	if(!temp.equals("equation = ")) {
+		System.out.println(temp);
+		//System.exit(0);
+		}
+	
+}
+void divide(int a[] , int val , int val1, int val2, String c1 ) {
+	
+String temp = "equation = ";
+int x = (a[2] * 10) + a[3];
 
-	void crossCheck(int a[], int val, int val1, int val2, String c1) {
-		plus(a, val, val1, val2, c1);
-		minus(a, val, val1, val2, c1);
-		multiple(a, val, val1, val2, c1);
-		divide(a, val, val1, val2, c1);
+	if( val >= (a[2] + a[3]) && val % (a[2] + a[3]) == 0 && val / (a[2] + a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "/" + "(" + a[2] + "+" + a[3] + ")";
+	}
+	else if(a[2] > a[3] && val >= (a[2] - a[3]) && val % (a[2] - a[3]) == 0 && val / (a[2] - a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "/" + "(" + a[2] + "-" + a[3] + ")";
+	}
+	else if(val >= (a[2] * a[3]) && val % (a[2] * a[3]) == 0 && val / (a[2] * a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "/" + "(" + a[2] + "*" + a[3] + ")";
+	}
+	else if(a[2] >= a[3] && a[2] % a[3] == 0 && val >= (a[2] / a[3]) && val % (a[2] / a[3]) == 0 && val / (a[2] / a[3]) == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "/" + "(" + a[2] + "/" + a[3] + ")";
+	}
+	
+	else if(val >= x && val % x == 0 && val / x == target) {
+		temp += "(" + val1 + c1 + val2 + ")" + "/" + "(" + a[2] + "" + a[3] + ")";
+	}
+	
+	if(!temp.equals("equation = ")) {
+		System.out.println(temp);
+		//System.exit(0);
+		}
+}
 
+	
+	void crossCheck(int a[] , int val , int val1, int val2, String c1 ) {
+		plus(a, val , val1 , val2 , c1);
+		minus(a, val , val1 , val2 , c1);
+		multiple(a, val , val1 , val2 , c1);
+		divide(a, val , val1 , val2 , c1);
+		
 	}
 
 }
